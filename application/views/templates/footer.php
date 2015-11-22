e62530ad6e7a1f5c2f27e4f963e8e0a3c56979ea
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2015-11-22 16:13:15 +0700

    editing header and footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -8,9 +8,6 @@
 		color: gray;
 	}
 	</style>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/scripts.js') ?>"></script>
 	<div class="copyright"><?php echo lang('copyright') ?></div>
   </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,25 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" type="image/gif" href="<?php echo base_url('assets/img/icon/glyphicons-book-open.png') ?>">
-    <title><?php echo lang('window_title') ?></title>
+        <link rel="icon" type="image/gif" href="<?php echo base_url('assets/img/icon/glyphicons-book-open.png') ?>">
+        <title><?php echo lang('window_title') ?></title>
 
-    <meta name="description" content="Source code generated using layoutit.com">
-    <meta name="ajhoel" content="E-Learning">
+        <meta name="description" content="Source code generated using layoutit.com">
+        <meta name="ajhoel" content="K-Lone">
 
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css') ?>" />
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" />
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>" />
-    <style type="text/css">
-	.body {
-    	margin-top: 5em;
-    }
-    </style>
-  </head>
-  <body class="body">
-  	<div class="container">
-  		<div class="row">
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+        <script src="<?php echo base_url('assets/js/jquery.js') ?>"></script>
+        <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+
+        <style type="text/css">
+        .body {
+        	margin-top: 5em;
+        }
+        </style>
+    </head>
+    <body class="body">
+      	<div class="container">
+      		<div class="row">
