5b8b8c553accb44ec470199ff2097b8c879e8c73
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2015-11-22 21:55:35 +0700

    editing lang

--- application/language/english/en_lang.php
+++ application/language/english/en_lang.php
@@ -4,23 +4,19 @@ $lang['window_title'] = 'K-Lone Web';
 $lang['main_title'] = "K-Lone Web Admin";
 $lang['copyright'] = "Copyright &copy 2015";
 
-$lang['label_no'] = 'No.';
-
 //Label
+$lang['label_forgot'] = 'Forgot Password?';
+$lang['label_hash'] = 'Hash';
+$lang['label_id'] = 'ID';
+$lang['label_jenis_user'] = 'Jenis User';
+$lang['label_login'] = 'Log In';
+$lang['label_logout'] = 'Log Out';
 $lang['label_master'] = 'Master Data';
-$lang['label_username'] = 'Username';
+$lang['label_no'] = 'No.';
 $lang['label_password'] = 'Password';
 $lang['label_salt'] = 'Salt';
-$lang['label_hash'] = 'Hash';
-$lang['label_jenis_user'] = 'Jenis User';
-
 $lang['label_user'] = "User";
-$lang['label_siswa'] = "Siswa";
-$lang['label_guru'] = "Guru";
-$lang['label_matpel'] = "Mata Pelajaran";
-$lang['label_kelas'] = "Kelas";
-$lang['label_kuis'] = "Kuis";
-$lang['label_tugas'] = "Tugas";
+$lang['label_username'] = 'Username';
 
 //Label button
 $lang['button_add'] = 'Add';
@@ -28,9 +24,6 @@ $lang['button_close'] = 'Close';
 $lang['button_delete'] = 'Delete';
 $lang['button_new'] = "New Input";
 $lang['button_update'] = 'Update';
-$lang['label_login'] = 'Log In';
-$lang['label_logout'] = 'Log Out';
-$lang['label_forgot'] = 'Forgot Password?';
 
 //Message
 $lang['message_error_update'] = 'Data can not be updated';
