f757158d9723d3b2616332ab71345de36dace8ec
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2015-12-01 21:41:19 +0700

    add label

--- application/language/english/en_lang.php
+++ application/language/english/en_lang.php
@@ -5,17 +5,21 @@ $lang['main_title'] = "K-Lone Web Admin";
 $lang['copyright'] = "Copyright &copy 2015";
 
 //Label
+$lang['label_birth'] = 'Birth Date';
+$lang['label_email'] = 'Email';
 $lang['label_forgot'] = 'Forgot Password?';
 $lang['label_hash'] = 'Hash';
 $lang['label_id'] = 'ID';
-$lang['label_jenis_user'] = 'Jenis User';
 $lang['label_login'] = 'Log In';
 $lang['label_logout'] = 'Log Out';
 $lang['label_master'] = 'Master Data';
 $lang['label_no'] = 'No.';
 $lang['label_password'] = 'Password';
+$lang['label_phone'] = 'Phone';
 $lang['label_salt'] = 'Salt';
+$lang['label_status'] = 'Status';
 $lang['label_user'] = "User";
+$lang['label_user_prev'] = 'Type';
 $lang['label_username'] = 'Username';
 
 //Label button
@@ -29,6 +33,23 @@ $lang['button_update'] = 'Update';
 $lang['message_error_update'] = 'Data can not be updated';
 $lang['message_error_insert'] = 'Data can not be inserted';
 $lang['message_error_delete'] = 'Data can not be deleted';
+$lang['message_success_insert'] = 'Data has been inserted';
+$lang['message_success_update'] = 'Data has been updated';
+$lang['message_success_delete'] = 'Data has been deleted';
+
+//Month
+$lang['januari'] = "Januari";
+$lang['februari'] = "Februari";
+$lang['maret'] = "Maret";
+$lang['april'] = "April";
+$lang['mei'] = "Mei";
+$lang['juni'] = "Juni";
+$lang['juli'] = "Juli";
+$lang['agustus'] = "Agustus";
+$lang['september'] = "September";
+$lang['oktober'] = "Oktober";
+$lang['november'] = "November";
+$lang['desember'] = "Desember";
 
 //Title
 
