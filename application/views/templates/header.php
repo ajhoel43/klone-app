d57bd2572427ad4ae42f27cabf2ee27a2cc73d56
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2015-12-11 23:22:57 +0700

    add font-awesome css

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -29,7 +29,7 @@
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         -->
-
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.css') ?>">
         <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
         <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
 
