59f5c418419b257aed84f68d99aca56463d17699
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2015-12-15 08:14:28 +0700

    add Custom Error Page

---
+++ application/views/pages/fail_page.php
@@ -0,0 +1,20 @@
+<style type="text/css">
+	.error-msg
+	{
+		display: flex;
+		justify-content: center;
+		align-items:center;
+		text-align:center;
+		padding-top: 12%;
+		padding-bottom: 5%;
+		font-size:50px;
+		opacity: 0.3;
+		filter: Alpha(opacity=30);
+		font-family: Courier New;
+		font-weight: bold;
+	}
+</style>
+<div class="error-msg">
+	ERROR: FAIL!!!!<br>
+	TESTING APA-APA
+</div>
