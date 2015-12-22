45078a5535b15973d7f968a143592191b6852e36
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2015-12-22 23:06:24 +0700

    modify hooks

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -229,17 +229,41 @@ function _valid_email($subject)
 }
 
 function _generateCode() 
+{
+	$length = 10;
+	$char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	$karakter = preg_replace("/[^A-Za-z0-9]/",'', $char);
+	//set string = null
+	$string = '';
+	for ($i = 0; $i < $length; $i++) 
 	{
-		$length = 10;
-		$char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		$karakter = preg_replace("/[^A-Za-z0-9]/",'', $char);
-		//set string = null
-		$string = '';
-		for ($i = 0; $i < $length; $i++) 
-		{
-			$pos = rand(0, strlen($karakter)-1);
-			$string .= $karakter{$pos};
-		}
-		
-		return $string;
+		$pos = rand(0, strlen($karakter)-1);
+		$string .= $karakter{$pos};
 	}
+	
+	return $string;
+}
+
+function load_pagination_config()
+{
+	$config['full_tag_open'] = '<ul class="pagination">';
+	$config['full_tag_close'] = '</ul>';
+	$config['first_link'] = '<span class="glyphicon glyphicon-chevron-left"></span><span class="glyphicon glyphicon-chevron-left"></span> First';
+	$config['last_link'] = 'Last <span class="glyphicon glyphicon-chevron-right"></span><span class="glyphicon glyphicon-chevron-right"></span>';
+	$config['prev_link'] = '<span class="glyphicon glyphicon-chevron-left"></span> Prev';
+	$config['next_link'] = 'Next <span class="glyphicon glyphicon-chevron-right"></span>';
+	$config['first_tag_open'] = '<li>';
+	$config['first_tag_close'] = '</li>';
+	$config['last_tag_open'] = '<li>';
+	$config['last_tag_close'] = '</li>';
+	$config['prev_tag_open'] = '<li>';
+	$config['prev_tag_close'] = '</li>';
+	$config['next_tag_open'] = '<li>';
+	$config['next_tag_close'] = '</li>';
+	$config['cur_tag_open'] = '<li class="active"><a>';
+	$config['cur_tag_close'] = '</a></li>';
+	$config['num_tag_open'] = '<li>';
+	$config['num_tag_close'] = '</li>';
+
+	return $config;
+}
