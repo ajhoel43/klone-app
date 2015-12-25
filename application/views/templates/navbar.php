7b9b4dfc065ebda8252328b10822c37e3a48276f
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2015-12-25 20:24:45 +0700

    editing navbar view

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -2,15 +2,25 @@
 .nav-margin {
 	margin-right: 1em;
 }
+.navbar-brand {
+  padding: 0px;
+}
+.navbar-brand > img {
+  max-height: 100%;
+  height: 100%;
+  padding: 5px 15px;
+  width:auto;
+  /*-o-object-fit: contain;
+  object-fit: contain;*/
+}
 </style>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/navbar.css') ?>">
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
 	<div class="navbar-header">
-		 
 		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 			 <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
 		</button>
-		<a class="navbar-brand" href="<?php echo base_url('') ?>"><span class="glyphicon glyphicon-home"></span> <?php echo lang('window_title') ?></a>
+		<a class="navbar-brand" href="<?php echo base_url('') ?>"><img src="<?php echo base_url('assets/img/logo/klonefont.png') ?>" class="nav-logo"></a>
 	</div>
 
 	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
