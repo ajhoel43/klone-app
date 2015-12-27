815cb281629d9742a86eb2ad730c7ede24f4fbe8
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2015-12-27 18:53:46 +0700

    add CRUD function for Master KOTA and add new select method for choosing wilayah

--- application/controllers/Wil.php
+++ application/controllers/Wil.php
@@ -27,6 +27,7 @@ class Wil extends CI_Controller {
 		}
 	}
 
+	// ********************* Function WIL ********************** //
 	function _validate_form_wil()
 	{
 		$this->form_validation->set_rules('kode_wil', lang('label_wil_kode'), 'required');
@@ -136,9 +137,142 @@ class Wil extends CI_Controller {
 		}
 	}
 
-	function info_jb()
+	function select_wil()
 	{
-		$data['recordsjb'] = $this->m_jb->get_list_jb();
-		echo $this->load->view('jb/info_jb', $data, TRUE);
+		$key = $this->input->post('keyword');
+		$data['records'] = $this->m_wil->get_list_wil(array('nama_wil' => $key));
+		$count = count($data['records']);
+
+		if($count > 0)
+			die($this->load->view('wil/select_wil', $data, TRUE));
+		else {
+			echo "<center><div class='alert alert-danger' style='margin-top:2em;'>No Records Found</div></center>";
+			die();
+		}
 	}
+	// ********************* END WILAYAH ************************ //
+
+	// ********************* Function KOTA ********************** //
+	function _validate_form_kota()
+	{
+		$this->form_validation->set_rules('kode_wil', lang('label_wil'), 'required');
+		$this->form_validation->set_rules('kode_kota', lang('label_kota_kode'), 'required|numeric');
+		$this->form_validation->set_rules('nama_kota', lang('label_kota'), 'required');
+
+		if($this->form_validation->run() === TRUE)
+			return true;
+		else
+			return false;
+	}
+	
+	function index_kota()
+	{
+		$this->load->library('pagination');
+		
+		$config = load_pagination_config();
+
+		$config['base_url'] = base_url('wil/index_kota');
+		$config['total_rows'] = count($this->m_wil->get_list_kota());
+		$config['per_page'] = 20;
+		// $choice = $config['total_rows'] / $config['per_page'];
+		// $config['num_links'] = round($choice);
+		
+		$this->pagination->initialize($config);
+
+		$page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+
+		$params = array();
+		$params['limit'] = $config['per_page'];
+		$params['start'] = $page;
+
+		$data = array();
+		$data['records'] = $this->m_wil->get_list_kota($params);
+		$data['links'] = $this->pagination->create_links();
+		$this->template1->create_view('wil/list_kota', $data);
+	}
+
+	function add_kota()
+	{
+		$submit = $this->input->post('submit');
+
+		if($submit)
+		{
+			unset($_POST['submit']);
+
+			$vresult = $this->_validate_form_kota();
+			if(!$vresult)
+				die(report_flag($this->error, validation_errors()));
+
+			// Generate ID format*
+			$_POST['ID_kota'] = join('', array($_POST['kode_wil'], $_POST['kode_kota']));
+			// ******************
+
+			list($bresult, $msg) = $this->m_wil->add_kota($this->input->post());
+
+			if(!$bresult)
+				die(report_flag($this->error, $msg));
+			else
+				die(report_flag($this->success, $msg));
+		}
+
+		$data['wil_dd'] = $this->m_wil->wil_dropdown();
+		$this->load->view('wil/add_kota', $data);
+	}
+
+	function upd_kota($id)
+	{
+		$submit = $this->input->post('submit');
+
+		if($submit)
+		{
+			unset($_POST['submit']);
+
+			if(!$this->_validate_form_kota())
+				die(report_flag($this->error, validation_errors()));
+
+			// Generate ID format*
+			$_POST['ID_kota'] = join('', array($_POST['kode_wil'], $_POST['kode_kota']));
+			// ******************
+
+			list($bresult, $msg) = $this->m_wil->add_kota($this->input->post(), $id);
+
+			if(!$bresult)
+				die(report_flag($this->error, $msg));
+			else
+				die(report_flag($this->success, $msg));
+		}
+
+		$data = array();
+		$data['record'] = $this->m_wil->get_one_kota(array('id' => $id));
+		$this->load->view('wil/upd_kota', $data);
+	}
+
+	function del_kota($id)
+	{
+		$bresult = $this->m_wil->del_kota($id);
+
+		if(!$bresult)
+			die(report_flag($this->error, delete_flag($bresult)));
+		else
+			die(report_flag($this->success, delete_flag($bresult)));
+	}
+
+	function search_kota()
+	{
+		$params = array(
+			'nama_kota' => $this->input->post('nama_kota'),
+			'nama_wil' => $this->input->post('nama_wil')
+			);
+		$data['records'] = $this->m_wil->get_list_kota($params);
+		$count = count($data['records']);
+
+		if($count > 0)
+			die($this->load->view('wil/list_kota_ajax', $data, TRUE));
+		else{
+			echo "<center><div class='alert alert-danger' style='margin-top:2em;'>No Records Found</div></center>";
+			die();
+		}
+	}
+	// ********************* END KOTA ************************ //
+
 }
