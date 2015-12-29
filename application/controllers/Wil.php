0220fb3176911a113b806b0818c3c32a27588734
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2015-12-29 21:42:12 +0700

    updating model & controller add CRUD + Search function for M_kecamatan

--- application/controllers/Wil.php
+++ application/controllers/Wil.php
@@ -48,9 +48,8 @@ class Wil extends CI_Controller {
 		$config['base_url'] = base_url('wil/index');
 		$config['total_rows'] = count($this->m_wil->get_list_wil());
 		$config['per_page'] = 20;
-		// $choice = $config['total_rows'] / $config['per_page'];
-		// $config['num_links'] = round($choice);
-		
+		$config['num_links'] = 5;
+
 		$this->pagination->initialize($config);
 
 		$page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
@@ -174,9 +173,8 @@ class Wil extends CI_Controller {
 		$config['base_url'] = base_url('wil/index_kota');
 		$config['total_rows'] = count($this->m_wil->get_list_kota());
 		$config['per_page'] = 20;
-		// $choice = $config['total_rows'] / $config['per_page'];
-		// $config['num_links'] = round($choice);
-		
+		$config['num_links'] = 5;
+
 		$this->pagination->initialize($config);
 
 		$page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
@@ -273,6 +271,147 @@ class Wil extends CI_Controller {
 			die();
 		}
 	}
+
+	function select_kota()
+	{
+		$key = $this->input->post('keyword');
+		if($this->input->post('s_by') == 'kota')
+			$params = array('nama_kota' => $key);
+		else
+			$params = array('nama_wil' => $key);
+
+		$data['records'] = $this->m_wil->get_list_kota($params);
+		$count = count($data['records']);
+
+		if($count > 0)
+			die($this->load->view('wil/select_kota', $data, TRUE));
+		else {
+			echo "<center><div class='alert alert-danger' style='margin-top:2em;'>No Records Found</div></center>";
+			die();
+		}
+	}
 	// ********************* END KOTA ************************ //
+
+	// ********************* Function KEC *********************//
+	function _validate_form_kec()
+	{
+		$this->form_validation->set_rules('kode_kec', lang('label_kec_kode'), 'required|numeric');
+		$this->form_validation->set_rules('ID_kota', lang('label_kota_kode'), 'required');
+		$this->form_validation->set_rules('nama_kec', lang('label_kec'), 'required');
+
+		if($this->form_validation->run() === TRUE)
+			return true;
+		else
+			return false;
+	}
+	
+	function index_kec()
+	{
+		$this->load->library('pagination');
+		
+		$config = load_pagination_config();
+
+		$config['base_url'] = base_url('wil/index_kec');
+		$config['total_rows'] = count($this->m_wil->get_list_kec());
+		$config['per_page'] = 10;
+		$config['num_links'] = 5;
+		
+		$this->pagination->initialize($config);
+
+		$page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+
+		$params = array();
+		$params['limit'] = $config['per_page'];
+		$params['start'] = $page;
+
+		$data = array();
+		$data['records'] = $this->m_wil->get_list_kec($params);
+		$data['links'] = $this->pagination->create_links();
+		$this->template1->create_view('wil/list_kec', $data);
+	}
+
+	function add_kec()
+	{
+		$submit = $this->input->post('submit');
+
+		if($submit)
+		{
+			unset($_POST['submit']);
+
+			$vresult = $this->_validate_form_kec();
+			if(!$vresult)
+				die(report_flag($this->error, validation_errors()));
+
+			// Generate ID format*
+			$_POST['ID_kec'] = join('', array($_POST['ID_kota'], $_POST['kode_kec']));
+			// ******************
+
+			list($bresult, $msg) = $this->m_wil->add_kec($this->input->post());
+
+			if(!$bresult)
+				die(report_flag($this->error, $msg));
+			else
+				die(report_flag($this->success, $msg));
+		}
+		$data = array();
+		$this->load->view('wil/add_kec', $data);
+	}
+
+	function upd_kec($id)
+	{
+		$submit = $this->input->post('submit');
+
+		if($submit)
+		{
+			unset($_POST['submit']);
+
+			if(!$this->_validate_form_kec())
+				die(report_flag($this->error, validation_errors()));
+
+			// Generate ID format*
+			$_POST['ID_kec'] = join('', array($_POST['ID_kota'], $_POST['kode_kec']));
+			// ******************
+
+			list($bresult, $msg) = $this->m_wil->add_kec($this->input->post(), $id);
+
+			if(!$bresult)
+				die(report_flag($this->error, $msg));
+			else
+				die(report_flag($this->success, $msg));
+		}
+
+		$data = array();
+		$data['record'] = $this->m_wil->get_one_kec(array('id' => $id));
+		$this->load->view('wil/upd_kec', $data);
+	}
+
+	function del_kec($id)
+	{
+		$bresult = $this->m_wil->del_kec($id);
+
+		if(!$bresult)
+			die(report_flag($this->error, delete_flag($bresult)));
+		else
+			die(report_flag($this->success, delete_flag($bresult)));
+	}
+
+	function search_kec()
+	{
+		$params = array(
+			'nama_kota' => $this->input->post('nama_kota'),
+			'nama_wil' => $this->input->post('nama_wil'),
+			'nama_kec' => $this->input->post('nama_kec')
+			);
+		$data['records'] = $this->m_wil->get_list_kec($params);
+		$count = count($data['records']);
+
+		if($count > 0)
+			die($this->load->view('wil/list_kec_ajax', $data, TRUE));
+		else{
+			echo "<center><div class='alert alert-danger' style='margin-top:2em;'>No Records Found</div></center>";
+			die();
+		}
+	}
+	// ********************* END Kecamatan ********************//
 
 }
--- application/models/M_wil.php
+++ application/models/M_wil.php
@@ -45,8 +45,6 @@ class M_wil extends CI_Model
 
 		if (isset($params['limit']) && isset($params['start']))
 			$this->db->limit($params['limit'], $params['start']);
-		else
-			$this->db->limit(20);
 
 		$this->db->order_by('kode_wil asc');
 		$query = $this->db->get('M_wilayah')->result();
@@ -128,8 +126,6 @@ class M_wil extends CI_Model
 
 		if (isset($params['limit']) && isset($params['start']))
 			$this->db->limit($params['limit'], $params['start']);
-		else
-			$this->db->limit(20);
 
 		$this->db->join('M_wilayah mw', 'mw.kode_wil = mk.kode_wil', 'left');
 
@@ -167,7 +163,8 @@ class M_wil extends CI_Model
 		$rec = $this->get_one_kota(array('id' => $params['ID_kota']));
 		$query = 0;
 		$msg = lang('label_kota_kode')." for <b>".$rec->nama_wil."</b>".lang('message_data_exist')." ";
-		$msg .= "<div style='text-align:right;'>=> <b>$rec->kode_kota</b>".lang('message_IsUse')."<b>".$rec->nama_kota."</b></div>";
+		$msg .= "<br><br>Report :";
+		$msg .= "<div style='text-align:right;'>".lang('label_kode')." => <b>$rec->kode_kota</b>".lang('message_IsUse')."<b>".$rec->nama_kota."</b></div>";
 
 		if($id)
 		{
@@ -199,5 +196,108 @@ class M_wil extends CI_Model
 		return $query;
 	}
 	// ********************** END KOTA ************************** //
+
+	// ********************** START KEC ************************** //
+	function kec_dropdown()
+	{
+		$buffer = array();
+		$buffer[''] = '- '.lang('label_kec').' -';
+
+		$this->db->join('M_kota mk', 'mk.ID_kota = mkec.ID_kota', 'left');
+		$this->db->join('M_wilayah mw', 'mk.kode_wil = mw.kode_wil', 'left');
+		$query = $this->db->get('M_kecamatan mkec')->result();
+
+		// foreach ($query as $q) {
+		// 	$buffer[$q->kode_kota] = $q->nama_wil." >> ".$q->nama_kota;
+		// }
+
+		return $buffer;
+	}
+
+	function get_list_kec($params = array())
+	{
+		if(isset($params['nama_kec']) && $params['nama_kec'] != '')
+			$this->db->like('mkec.nama_kec', $params['nama_kec']);
+
+		if(isset($params['nama_kota']) && $params['nama_kota'] != '')
+			$this->db->like('mk.nama_kota', $params['nama_kota']);
+
+		if(isset($params['nama_wil']) && $params['nama_wil'] != '')
+			$this->db->like('mw.nama_wil', $params['nama_wil']);
+
+		if (isset($params['limit']) && isset($params['start']))
+			$this->db->limit($params['limit'], $params['start']);
+
+		$this->db->join('M_kota mk', 'mk.ID_kota = mkec.ID_kota', 'left');
+		$this->db->join('M_wilayah mw', 'mw.kode_wil = mk.kode_wil', 'left');
+
+		$this->db->order_by('mkec.ID_kec asc');
+		$query = $this->db->get('M_kecamatan mkec')->result();
+
+		return $query;
+	}
+
+	function get_one_kec($params = array())
+	{
+		if(isset($params['id']))
+			$this->db->where('mkec.ID_kec', $params['id']);
+		
+		$this->db->join('M_kota mk', 'mk.ID_kota = mkec.ID_kota', 'left');
+		$this->db->join('M_wilayah mw', 'mw.kode_wil = mk.kode_wil', 'left');
+		$query = $this->db->get('M_kecamatan mkec')->row();
+
+		return $query;
+	}
+
+	function get_max_kec($params = array())
+	{
+		// $this->db->select('');
+		$this->db->where('ID_kota', $params['kode_kota']);
+		$this->db->select_max('kode_kec', 'ID');
+
+		$query = $this->db->get('M_kecamatan')->row();
+
+		return $query;
+	}
+
+	function add_kec($params, $id = null)
+	{
+		// Manual check for same id record
+		$rec = $this->get_one_kec(array('id' => $params['ID_kec']));
+		$query = 0;
+		$msg = lang('label_kec_kode')." for ".lang('label_kota')." <b>".$rec->nama_kota."</b>".lang('message_data_exist')." ";
+		$msg .= "<br><br>Report :";
+		$msg .= "<div style='text-align:left;'>".lang('label_kode')." => <b>$rec->kode_kec</b>".lang('message_IsUse')."<b>".$rec->nama_kec."</b></div>";
+
+		if($id)
+		{
+			$rec1 = $this->get_one_kec(array('id' => $id));
+			if($rec1->ID_kota == $params['ID_kec'] OR is_null($rec))
+			{
+				$this->db->where('ID_kec', $id);
+				$query = $this->db->update('M_kecamatan', $params);
+				$msg = update_flag($query);
+			}
+		}
+		else
+		{
+			if(is_null($rec))
+			{
+				$query = $this->db->insert('M_kecamatan', $params);
+				$msg = insert_flag($query);
+			}
+		}
+
+		return array($query, $msg);
+	}
+
+	function del_kec($id)
+	{
+		$this->db->where('ID_kec', $id);
+		$query = $this->db->delete('M_kecamatan');
+
+		return $query;
+	}
+	// ********************** END KEC **************************** //
 
 }
