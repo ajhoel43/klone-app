652d43cfde6cd60fb3a89714a9601d8f401dac98
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2016-01-08 09:36:38 +0700

    update view

--- application/views/jeken/list_data_ajax.php
+++ application/views/jeken/list_data_ajax.php
@@ -3,7 +3,9 @@
 	<tr class="active">
 		<th style="width:3%;"><?php echo lang('label_no') ?></th>
 		<th style="width:5%; "><?php echo lang('label_action') ?></th>
+		<th width="10%"><?php echo lang('label_kend_type') ?></th>
 		<th width="12%"><?php echo lang('label_jeken') ?></th>
+		<th width="10%"><?php echo lang('label_cc') ?></th>
 		<th><?php echo lang('label_desc') ?></th>
 	</tr>
 	<?php $no = 1; ?>
@@ -21,7 +23,22 @@
 					</ul>
 				</div>
 			</td>
+			<td><strong><?php echo ucfirst(substr($record->kode_jeken, 0, 5)) ?></strong></td>
 			<td><?php echo $record->nama_jeken ?></td>
+			<td style="text-align:center;">
+				<?php 
+					if($record->cc_max != 0)
+					{
+						$ccmax = $record->cc_max;
+						echo join('cc ~ ', array($record->cc_min, $ccmax))."cc";
+					}
+					else
+					{
+						$ccmax = lang('label_cc_up');
+						echo join('cc ', array($record->cc_min, $ccmax));
+					}
+				?>
+			</td>
 			<td><?php echo $record->deskripsi ?></td>
 		</tr>
 	<?php $no++; endforeach; ?>
